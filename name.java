4ae0ac09360ed2b910a431b8ea3863baa16517cc
SANTHOSHINI M <santhoshini2901@gmail.com>
2026-09-24 22:23:08 +0530

    Add First Java Program

---
+++ name.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class name{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String Name = sc.nextLine();
+        String address = sc.nextLine();
+        int Age = sc.nextInt();
+        System.out.println("Name:"+Name);
+        System.out.println("Age:"+Age);
+        System.out.println("Address:"+address);
+
+    }
+}
